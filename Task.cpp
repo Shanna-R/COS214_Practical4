40808efa1bd3de6a62da82a30301d8f91e2d632d
ShannaR6 <u25008260@tuks.co.za>
2026-09-07 09:02:16 +0200

    Updated files

--- Task.cpp
+++ Task.cpp
@@ -1,9 +1,11 @@
 #include "Task.h"
+#include "TaskState.h"
+#include "TodoState.h"
 
 #include <iostream>
+#include <memory>
 
-Task::Task(const std::string& name)
-    : name(name)
+Task::Task(const std::string& name) : name(name), state(std::unique_ptr<TaskState>(new TodoState()))
 {
 }
 
@@ -18,15 +20,43 @@ std::string Task::getName() const
 
 void Task::display(int depth) const
 {
-    for (int i = 0; i < depth; ++i)
+    for(int i = 0; i < depth; ++i)
     {
         std::cout << "    ";
     }
 
-    std::cout << "- " << name << std::endl;
+    std::cout << "- " << name << " [" << getStatus() << "]" << std::endl;
 }
 
 bool Task::isGroup() const
 {
     return false;
+}
+
+void Task::start()
+{
+    state->start(*this);
+}
+
+void Task::complete()
+{
+    state->complete(*this);
+}
+
+void Task::execute()
+{
+    state->execute(*this);
+}
+
+std::string Task::getStatus() const
+{
+    return state->getStatus();
+}
+
+void Task::setState(std::unique_ptr<TaskState> newState)
+{
+    if(newState)
+    {
+        state = std::move(newState);
+    }
 }
--- Task.h
+++ Task.h
@@ -3,10 +3,16 @@
 
 #include "TaskComponent.h"
 
+#include <memory>
+#include <string>
+
+class TaskState;
+
 class Task : public TaskComponent
 {
     private:
         std::string name;
+        std::unique_ptr<TaskState> state;
 
     public:
         Task(const std::string& name);
@@ -15,6 +21,16 @@ class Task : public TaskComponent
         virtual std::string getName() const override;
         virtual void display(int depth = 0) const override;
         virtual bool isGroup() const override;
+
+        // State-related behaviour
+        void start();
+        void complete();
+        void execute();
+
+        std::string getStatus() const;
+
+        // Used by concrete states to change the Context's state
+        void setState(std::unique_ptr<TaskState> newState);
 };
 
 #endif
